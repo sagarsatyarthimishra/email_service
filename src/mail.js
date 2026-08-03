97bbad39720e3ad94e521334d7c282f6e657e0bd
Sagar Satyarthi Mishra <sagarsatyarthimishra@gmail.com>
2026-08-03 22:12:53 +0530

    emial

--- src/mail.js
+++ src/mail.js
@@ -2,11 +2,16 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS after connection
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 module.exports = transporter;
